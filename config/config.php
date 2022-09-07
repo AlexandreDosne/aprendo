a9a819255c1c40e9a4457a8ac1ca09f92a41762b
Alexandre Dosne <alexandre@dosne.com>
2022-09-07 03:20:24 +0200

    Updated a few files

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_regenerate_id();
-define('APP_ROOT', dirname(__DIR__) . '/');
-define('PATH_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
--- public/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-/* let nav = document.getElementById('navbar');
-window.onscroll = function () {
-    if (window.pageYOffset > 10) {
-        nav.classList.add("fixed-top");
-    } else {
-        nav.classList.remove("fixed-top");
-    }
-} */
